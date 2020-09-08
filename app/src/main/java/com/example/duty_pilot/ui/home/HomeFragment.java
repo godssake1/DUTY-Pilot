f86edddfcb0773d06153f592a1e2287f819ec7b5
unknown <kjung10@gmail.com>
2020-09-08 17:39:25 +0900

    settings fragment added and modified

--- app/src/main/java/com/example/duty_pilot/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/duty_pilot/ui/home/HomeFragment.java
@@ -12,11 +12,41 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.duty_pilot.BlankFragment;
 import com.example.duty_pilot.R;
 
 public class HomeFragment extends Fragment {
 
+    // Arguments related
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private String mParam1;
+
+
+
+    // Constructor
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    // newInstance
+    public static HomeFragment newInstance(String param1) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     private HomeViewModel homeViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +54,6 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.tv_today);
-
 
         /*
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
--- app/src/main/java/com/example/duty_pilot/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/duty_pilot/ui/settings/SettingsFragment.java
@@ -13,16 +13,45 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.duty_pilot.R;
+import com.example.duty_pilot.ui.home.HomeFragment;
 
 public class SettingsFragment extends Fragment {
 
+    // Arguments related
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private String mParam1;
+
+    // Constructor
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    // newInstance
+    public static SettingsFragment newInstance(String param1) {
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     private SettingsViewModel settingsViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        /*
         final TextView textView = root.findViewById(R.id.text_dashboard);
         settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -30,6 +59,7 @@ public class SettingsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        */
         return root;
     }
 }
